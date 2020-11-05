93308ce37ad97ce48d2c8b5ac1f2d1a248163e16
FlavienLoiseau <flavien.loiseau@edu.escp.eu>
2020-11-05 14:39:56 +0100

    correctif création évènement

--- db/migrate/20201103131933_create_events.rb
+++ db/migrate/20201103131933_create_events.rb
@@ -1,7 +1,8 @@
 class CreateEvents < ActiveRecord::Migration[6.0]
   def change
     create_table :events do |t|
-      t.belongs_to :admin, index: true
+      t.belongs_to :user, index: true
+      t.references :admin, index: true
 
       t.datetime :start_date
       t.integer :duration
